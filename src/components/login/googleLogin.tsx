0dff3d5eb3ce0c8251eb80044366ae1ede6e733d
ZetBe <seoheewon123@hufs.ac.kr>
2025-05-07 22:36:12 +0900

    fix: 정적으로 링크 적용

--- src/components/login/googleLogin.tsx
+++ src/components/login/googleLogin.tsx
@@ -2,17 +2,15 @@
 
 import { Button } from '@mui/material'
 import styles from '@/shared/ui/login/loginPageStyles'
-import { useBaseUrl } from '@/shared/api/getBaseUrl'
 
 export default function GoogleLogin() {
-  const { data: baseUrl } = useBaseUrl()
   return (
     <Button
       fullWidth
       variant="outlined"
       sx={styles.googleButton}
       onClick={() => {
-        window.location.href = `${baseUrl}` + '/auth/oauth/google'
+        window.location.href = 'http://suehyun.kro.kr/auth/oauth/google'
       }}
     >
       구글로 로그인
