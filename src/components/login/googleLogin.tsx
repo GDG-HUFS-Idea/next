4d6e8c1b8d0d5fd90b60622bd61d13a9118fbdca
ZetBe <seoheewon123@hufs.ac.kr>
2025-05-17 14:10:53 +0900

    Revert "fix: 환경변수 문제 nextjs 버전 낮추어 해결 시도"
    This reverts commit 80e300bf5d04cfe9fac43757c1695b55d86e18ad.

--- src/components/login/googleLogin.tsx
+++ src/components/login/googleLogin.tsx
@@ -2,18 +2,15 @@
 
 import { Button } from '@mui/material'
 import styles from '@/shared/ui/login/loginPageStyles'
-import { env } from 'next-runtime-env'
 
 export default function GoogleLogin() {
-  const apiBaseUrl = env('NEXT_PUBLIC_API_BASE_URL')
-
   return (
     <Button
       fullWidth
       variant="outlined"
       sx={styles.googleButton}
       onClick={() => {
-        window.location.href = `${apiBaseUrl}/auth/oauth/google`
+        window.location.href = 'http://suehyun.kro.kr/auth/oauth/google'
       }}
     >
       구글로 로그인
